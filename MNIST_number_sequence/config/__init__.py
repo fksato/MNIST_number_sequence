0f66dbc09a1a9aaaa0ee188e666b776bb32192d7
fksato <f.k.sato@tum.de>
2019-09-28 22:43:34 -0700

    Update config.py

--- MNIST_number_sequence/config/__init__.py
+++ MNIST_number_sequence/config/__init__.py
@@ -1,7 +1,6 @@
 import os
-import sys
 
-ROOT_DIR = os.path.dirname(os.path.abspath(sys.modules['__main__'].__file__))
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 if not os.path.isdir(os.path.join(ROOT_DIR, 'datasets')):
 	os.mkdir(os.path.join(ROOT_DIR, 'datasets'))
